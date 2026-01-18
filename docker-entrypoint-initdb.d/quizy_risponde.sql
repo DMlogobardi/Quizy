4963960fc317d53f4e187cf6659c3fe09d59f088
DMlogobardi <davideninolongobardi@gmail.com>
2026-01-18 17:59:43 +0100

    fix: docker configuration

---
+++ docker-entrypoint-initdb.d/quizy_risponde.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: quizy
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `risponde`
+--
+
+DROP TABLE IF EXISTS `risponde`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `risponde` (
+  `id_risponde` int NOT NULL AUTO_INCREMENT,
+  `id_utente` int NOT NULL,
+  `id_risposta` int NOT NULL,
+  `quiz` varchar(200) NOT NULL,
+  `scelto_il` datetime(6) DEFAULT NULL,
+  `id_fa` int DEFAULT NULL,
+  PRIMARY KEY (`id_risponde`),
+  KEY `id_utente` (`id_utente`),
+  KEY `id_risposta` (`id_risposta`),
+  KEY `FK88k9y7n755cfj7s17qbplrgvc` (`id_fa`),
+  CONSTRAINT `FK88k9y7n755cfj7s17qbplrgvc` FOREIGN KEY (`id_fa`) REFERENCES `fa` (`id_fa`),
+  CONSTRAINT `risponde_ibfk_1` FOREIGN KEY (`id_utente`) REFERENCES `utente` (`id_utente`) ON DELETE CASCADE,
+  CONSTRAINT `risponde_ibfk_2` FOREIGN KEY (`id_risposta`) REFERENCES `risposta` (`id_risposta`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-01-17 18:28:19
